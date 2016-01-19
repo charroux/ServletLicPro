b8ca4223cf9fa0f3bf099f21c8a9b6ab616a0829
charroux <benoit.charroux@free.fr>
2016-01-19 16:35:46 +0100

    JSP avec un bean

---
+++ src/main/java/tp1/Evaluation.java
@@ -0,0 +1,23 @@
+package tp1;
+
+public class Evaluation {
+	String nom;
+	String note;
+	
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getNote() {
+		return note;
+	}
+	public void setNote(String note) {
+		this.note = note;
+	}
+	
+	
+
+}
---
+++ src/main/java/tp1/ServletEtudiant.java
@@ -0,0 +1,43 @@
+package tp1;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletEtudiant
+ */
+public class ServletEtudiant extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletEtudiant() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String nom = request.getParameter("nom");
+		String note = request.getParameter("note");
+		Evaluation evaluation = new Evaluation();
+		evaluation.setNom(nom);
+		evaluation.setNote(note);
+		request.setAttribute("etudiant", evaluation);
+		getServletContext().getRequestDispatcher("/etudiant.jsp").forward(request, response);
+	}
+
+}
