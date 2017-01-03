1cf5c7812ef2c5f74528166579d9444a1dc25651
charroux <benoit.charroux@free.fr>
2017-01-03 12:31:38 +0100

    servlet avec liste de beans

---
+++ src/main/java/form/Student.java
@@ -0,0 +1,27 @@
+package form;
+
+public class Student {
+
+	String value;
+	
+	public Student(String value) {
+		this.value = value;
+	}
+	
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+	
+	
+	
+
+}
