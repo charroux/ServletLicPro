3a4f44a7b3302ae3a8c411eff335f93d7e6cb6bc
charroux <benoit.charroux@free.fr>
2017-12-07 11:39:25 +0100

    ajout gretty

--- src/main/java/perso/Formulaire.java
+++ src/main/java/perso/Formulaire.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
 public class Formulaire extends HttpServlet{
 
 	@Override
@@ -14,13 +15,17 @@ public class Formulaire extends HttpServlet{
 		String nom = req.getParameter("nom");
 		System.out.println("nom = " + nom);
 		
-		String age = req.getParameter("age");
+		String age = req.getParameter("age"); // getParameter retourne des String !
 		System.out.println("age = " + age);
 		
 		//resp.sendRedirect("reponseStatique.html");
-		req.setAttribute("nom", nom);
+/*		req.setAttribute("nom", nom);
 		req.setAttribute("age", age);
 		getServletContext().getRequestDispatcher("/reponseDynamique.jsp").forward(req, resp);
+*/		
+		Personne pers = new Personne(nom, age);
+		req.setAttribute("personne", pers);
+		getServletContext().getRequestDispatcher("/jspBean.jsp").forward(req, resp);
 	}
 	
 }
